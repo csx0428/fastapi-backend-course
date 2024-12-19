31d959a061d1c9b2c99ef56265d146efa59d565f
diamlagou <zhentatong@gmail.com>
2024-12-19 15:36:38 +0800

    add-user-module

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from alembic import context
 
 from app.database import Base
 from app.models import Todo
-
+from app.models import User
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
